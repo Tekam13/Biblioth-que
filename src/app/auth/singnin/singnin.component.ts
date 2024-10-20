d33eb7926c1693e6aa7b95db219dcd113cc004e0
Tekam13 <=>
2024-10-20 18:50:49 +0100

    Livres

---
+++ src/app/auth/singnin/singnin.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-singnin',
+  standalone: true,
+  imports: [],
+  templateUrl: './singnin.component.html',
+  styleUrl: './singnin.component.css'
+})
+export class SingninComponent {
+
+}
